f58020d4293227db261b7a513312d213cd8b8402
Kyle Thomas <kthomas@Kyles-MacBook-Pro-2.local>
2019-10-14 09:48:04 +1100

    Add contact.php
    Added contact.php file

--- contact.php
+++ contact.php
@@ -1,1 +1,10 @@
-contact.php
+<?php // Template Name: Contact ?>
+<?php get_header(); ?>
+
+<?php echo do_shortcode('[contact-form-7 id="2" title="Contact"]'); ?>
+
+<script>
+  $(document).ready(function() {
+      $("#dynamic-fields").prepend("<input type='text' name='dynamic-field' id='dynamic-field'>");
+  });
+</script>
